f7ca8b102ee98fa536854e2efca38f41275e1aa1
Carol Castro <Leena@0587356883.wireless.umich.net>
2018-02-06 09:54:07 -0500

    twitter

--- secrets.py
+++ secrets.py
@@ -1,2 +1,5 @@
-consumer_key = "VIQjv1OJfTbKGNNvUppQcY9Jm"
-consumer_secret = "oe6hocvmyF9oEiVc1Juv8EB0OdTEcTaMCrRGw9ZBCnTFLNvvLR"
+client_key = "VIQjv1OJfTbKGNNvUppQcY9Jm"
+client_secret = "oe6hocvmyF9oEiVc1Juv8EB0OdTEcTaMCrRGw9ZBCnTFLNvvLR"
+
+access_token = "960875263662919680-mABtSiQmgyq3HgwbMPo1umziga5tWMJ"
+access_token_secret = "uPmSW8JSNNbc4ckqyRFCjGXHlRxSkGCg0UBL5eVM4cpX7"
